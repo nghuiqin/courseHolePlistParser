3e127eecfd72b556c8445277523416734e099fbc
Ng Hui Qin <huiqin.my@gmail.com>
2017-11-24 08:59:13 +0800

    sort out of longitude and latitude by holes

--- plistParser.py
+++ plistParser.py
@@ -6,7 +6,23 @@ namespace = '{http://www.opengis.net/kml/2.2}'
 document = root.find(namespace + 'Document')
 for item in document.findall(namespace + 'Placemark') :
     name = item.find(namespace + 'name')
-    print name.text
     coordinates = item.find(namespace + 'Polygon').find(namespace + 'outerBoundaryIs').find(namespace + 'LinearRing').find(namespace + 'coordinates')
-    print  coordinates.text
+    array = coordinates.text.split(',0')
+
+    longitude = []
+    latitude = []
+    for coordinate in array :
+         location = coordinate.strip().split(',')
+         long = location[0]
+         lat = location[-1]
+         longitude.append(long)
+         latitude.append(lat)
+
+    print name.text + '_latitude'
+    for number in latitude :
+        print number
+
+    print name.text + '_longitude'
+    for number in longitude :
+        print number
 
